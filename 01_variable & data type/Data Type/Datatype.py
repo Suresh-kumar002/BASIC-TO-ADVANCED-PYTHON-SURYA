88d94f3db744a149d13078a4dcc3c3712ff521bf
Suresh kumar <officialsuru2983@gmail.com>
2025-08-28 05:05:35 -0700

    Add files via upload

---
+++ 01_variable & data type/Data Type/Datatype.py
@@ -0,0 +1,27 @@
+a=complex(5,7)
+b=True
+c="sudarshn"
+d=None
+print(a)
+print(b)
+a1=5
+print(a+a1)
+print("the type of a",type(a))
+print("the type of b is",type(b))
+print("the type of c is",type(c))
+print("the type of d is",type(d))
+
+#list data type
+list=[6,8,7,[-5,7],["apple","banna"]]
+print(list)
+
+#tuple data type
+tuple=(("parrot","sparrow"),("lion","tiger"))
+print(tuple)
+
+#dictionary data type
+dic={"name":"suryaa","age":18,"canvote":true}
+print(dic)
+
+
+
---
+++ 01_variable & data type/Type casting/typecasting.py
@@ -0,0 +1,20 @@
+#Explicit type casting in python
+string="15"
+number = 7
+string_number=int(string)
+sum=number+string_number
+print("the sum of both number is:",sum)
+
+
+#Inplicit typecasting in python
+
+#python automatically conversion a to int
+a=7
+print(type(a))
+b=8.7
+print(type(b))
+
+c=a+b
+print(c)
+print(type(c))
+
